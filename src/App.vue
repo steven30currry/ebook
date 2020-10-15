e79785aeaf450963f3119aba20b85748075195b3
cyk <1248992865@qq.com>
2020-10-15 21:33:23 +0800

    vscode第一次项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <span class="icon-back"></span>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+document.addEventListener('DOMContentLoader', () => {
+  const html = document.querySelector('html')
+  html.style.fontSize = window.innerWidth / 10 + 'px'
+})
+</script>
+<style>
+
+</style>
